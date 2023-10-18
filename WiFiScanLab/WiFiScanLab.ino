464af45a1c81a82a095f1435e056506ee001179a
Ishana-Dewmini <ishanadewmini@gmail.com>
2023-10-18 14:48:44 +0530

    updated

--- WiFiScanLab/WiFiScanLab.ino
+++ WiFiScanLab/WiFiScanLab.ino
@@ -4,7 +4,7 @@
 
 const char *SSID = "GA20s";
 const char *PASSWORD = "anud1237";
-const char *mqttBroker = "broker.hivemq.com";
+const char *mqttBroker = "test.mosquitto.org";
 const int mqttPort = 1883;
 const char *mqttClientID = "Esp32_group07_2";
 const char *Topic = "Group071";
@@ -77,34 +77,42 @@ void loop() {
   } else if (scanResult > 0) {
     Serial.printf(PSTR("%d networks found:\n"), scanResult);
 
+    // Create a JSON array to hold network data
+    StaticJsonDocument<2500> doc; // Adjust the size as needed
+    JsonArray jsonArray = doc.to<JsonArray>();
+
+    // Iterate through the scanned networks
     for (int8_t i = 0; i < scanResult; i++) {
+      if (i==4){
+        break;
+      }
       WiFi.getNetworkInfo(i, ssid, encryptionType, rssi, bssid, channel, hidden);
 
       String rssiString = String(rssi);
       String bssidString = bssidToString(bssid);
 
-      // Create a JSON object for the network
-      StaticJsonDocument<200> doc;
-      JsonObject networkObject = doc.to<JsonObject>();
+      // Create a JSON object for each network
+      JsonObject networkObject = jsonArray.createNestedObject();
       networkObject["index"] = i;
       networkObject["bssid"] = bssidString;
       networkObject["rssi"] = rssiString;
-
-      // Serialize the JSON object to a string
-      String jsonString;
-      serializeJson(doc, jsonString);
-
-      if (!client.connected()) {
-        reconnect();
-      }
-
-      // Publish the JSON data for the network
-      client.publish(Topic, jsonString.c_str(), 2);
-      client.loop();
-      Serial.println(jsonString);
-
-      delay(1000); // Add a delay to control the publishing rate
     }
+
+    // Serialize the JSON array to a string
+    String jsonString;
+    serializeJson(doc, jsonString);
+
+    if (!client.connected()) {
+      reconnect();
+    }
+    // Publish the JSON data
+    client.publish(Topic, jsonString.c_str(), 2);
+    client.loop();
+    Serial.println(jsonString);            
+    
+
+    
+    
   } else {
     Serial.printf(PSTR("WiFi scan error %d"), scanResult);
   }
